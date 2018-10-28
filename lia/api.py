9d2cef3fb4aab57c853d38c8097662d8c7bb89e4
svigelj <aljaz.svigelj@gmail.com>
2018-10-28 19:21:59 +0100

    Rebased to new API and added basic bot

--- lia/api.py
+++ lia/api.py
@@ -5,22 +5,36 @@ class Api:
     def __init__(self, uid):
         self.response = {}
         self.uid = uid
+        self.current_index = 0
         self.type = "RESPONSE"
-        self.thrustSpeedEvents = []
+        self.speedEvents = []
         self.rotationEvents = []
         self.shootEvents = []
+        self.navigationStartEvents = []
+        self.navigationStopEvents = []
 
-    def set_thrust_speed(self, unit_id, speed):
-        self.thrustSpeedEvents.append({"unitId": unit_id, "speed": speed.name})
+    def get_index(self):
+        index = self.current_index
+        self.current_index += 1
+        return index
 
-    def set_rotation_speed(self, unit_id, rotation):
-        self.rotationEvents.append({"unitId": unit_id, "rotation": rotation.name})
+    def set_speed(self, unit_id, speed):
+        self.speedEvents.append({"index": self.get_index(), "unitId": unit_id, "speed": speed.name})
+
+    def set_rotation(self, unit_id, rotation):
+        self.rotationEvents.append({"index": self.get_index(), "unitId": unit_id, "rotation": rotation.name})
 
     def shoot(self, unit_id):
-        self.shootEvents.append({"unitId": unit_id})
+        self.shootEvents.append({"index": self.get_index(), "unitId": unit_id})
+
+    def navigation_start(self, unit_id, x, y):
+        self.navigationStartEvents.append({"index": self.get_index(), "unitId": unit_id, "x": x, "y": y})
+
+    def navigation_stop(self, unit_id):
+        self.navigationStopEvents.append({"index": self.get_index(), "unitId": unit_id})
 
 
-class ThrustSpeed(Enum):
+class Speed(Enum):
     NONE = 1
     FORWARD = 2
     BACKWARD = 3
@@ -29,4 +43,6 @@ class ThrustSpeed(Enum):
 class Rotation(Enum):
     NONE = 1
     LEFT = 2
-    RIGHT = 3
+    SLOW_LEFT = 3
+    RIGHT = 4
+    SLOW_RIGHT = 5
--- my_bot.py
+++ my_bot.py
@@ -1,20 +1,57 @@
 import asyncio
+import random
 
 from lia.callable import Bot
 from lia.networking_client import connect
-from lia.api import *
 
 
+# This is your bot. Implement it with your logic.
+#
+# In this example bot keeps sending units to random locations on map
+# and is able to shoot when it sees an opponent, it does not aim at
+# him though!
 class MyBot(Bot):
-    
-    def process_map_data(self, map_data):
-        print(map_data)
 
+    # Called only once when the game is initialized holding data about the map.
+    def process_map_data(self, map_data):
+        # We store the map that game uses in our variable.
+        # https://docs.liagame.com/api/#mapdata for the data you receive here
+        self.map = map_data["map"]
+
+    # Repeatedly called 10 times per second from game engine with
+    # game state updates. Here is where all the things happen.
     def process_state(self, state_update, api):
+
+        # Iterate through the data of all of your units
         for unit in state_update["units"]:
-            api.shoot(unit["id"])
+
+            # navigationPath is a field of your unit that shows the path
+            # on which the unit is currently using if you have sent it
+            # to a location using a api.navigationStart method.
+            # If it is empty it means there is no path set. In this case
+            # we choose a new destination and send the unit there.
+            if len(unit["navigationPath"]) == 0:
+                # Generate a point on map
+                x, y = self.random_valid_point_on_map()
+                # Send the unit to chosen point
+                api.navigation_start(unit["id"], x, y)
+
+            # If the unit has an opponent in it's viewing area then make it shoot
+            if len(unit["opponentsInView"]) > 0:
+                api.shoot(unit["id"])
+
+    # Finds a random point on the map that is not on the obstacle
+    def random_valid_point_on_map(self):
+        min_distance_to_map_edge = 2
+
+        # Generate new x and y until you get one that is not placed on an obstacle
+        while True:
+            x = random.randint(min_distance_to_map_edge, len(self.map) - min_distance_to_map_edge)
+            y = random.randint(min_distance_to_map_edge, len(self.map[0]) - min_distance_to_map_edge)
+            # if map at (x,y) is False it means there is no obstacle on that field
+            if self.map[x][y] is False:
+                return x, y
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(connect(MyBot()))
-
